d37f955a5b47475faf2a37c7c3b35ce2385e22ea
Soumyadeep Dey <soumyadeep.dey162003@gmail.com>
2025-02-21 11:10:59 +0530

    setup.py and equirements.txt updated

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path : str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup (
     name = 'ML_PRoject',
@@ -6,6 +19,6 @@ setup (
     author = 'Soumyadeep Dey',
     email = 'soumyadeep.dey162003@gmail.com',
     packages = find_packages(),
-    install_requires = ['pandas', 'numpy', 'scikit-learn', 'matplotlib', 'seaborn', 'tensorflow', 'keras'],
+    install_requires = get_requirements('requirements.txt'),
     license = 'MIT'
 )
